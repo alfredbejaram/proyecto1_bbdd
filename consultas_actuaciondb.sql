475000dbef7a3247d0bbb4761391edcf11ac44ba
alfredbejaram <fredbejara@gmail.com>
2025-03-31 14:56:23 +0200

    Resolvi consultas de la 8 a la 13

--- consultas_actuaciondb.sql
+++ consultas_actuaciondb.sql
@@ -139,6 +139,99 @@ WHERE duracion > 60;
 
 
 
-9. Mostrar el nombre y el argumento de las series emitidas en HBOM 
-10. Mostrar el total de las series que protagonice “Leonardo Di Caprio” 
-11. Calcular la media del precio de las plataformas “Semestrales” 
+/* 9. Mostrar el nombre y el argumento de las series emitidas en HBOM */
+
+SELECT s.nombre_serie AS 'Nombre de la serie', s.argumento AS 'Argumento'
+FROM series s
+INNER JOIN cartelera c ON s.cod_serie = c.cod_serie
+WHERE c.id_plataforma = 'HBOM';
+
+
++--------------------+-------------------------------------------------------------------------------------------------------------------------+
+| Nombre de la serie | Argumento                                                                                                               |
++--------------------+-------------------------------------------------------------------------------------------------------------------------+
+| Stranger Things    | En los a├▒os 80, un grupo de ni├▒os en Hawkins, Indiana, se enfrenta a fen├│menos paranormales y criaturas aterradoras.    |
++--------------------+-------------------------------------------------------------------------------------------------------------------------+
+1 row in set (0.001 sec)
+
+
+
+/* 10. Mostrar el total de las series que protagonice “Leonardo Di Caprio” */
+
+SELECT a.nombre_act AS 'Nombre de actor', COUNT(s.nombre_serie) AS 'Series como Protagonista'
+FROM series s
+INNER JOIN actores a ON s.actor_prota = a.actor_prota
+WHERE a.nombre_act = 'Leonardo DiCaprio';
+
++-------------------+--------------------------+
+| Nombre de actor   | Series como Protagonista |
++-------------------+--------------------------+
+| Leonardo DiCaprio |                        2 |
++-------------------+--------------------------+
+1 row in set (0.000 sec)
+
+
+/* 11. Calcular la media del precio de las plataformas “Semestrales” */
+
+SELECT AVG(precio) AS 'Precio Promedio Semestrales'
+FROM plataformas
+WHERE tipo_suscripcion = 'semestral';
+
++-----------------------------+
+| Precio Promedio Semestrales |
++-----------------------------+
+|                         110 |
++-----------------------------+
+1 row in set (0.000 sec)
+
+
+
+/* 12. Calcular la media del precio de las plataformas “Semestrales” emitidas después del 01/01/2022 */
+
+SELECT AVG(p.precio) AS 'Precio Prom Sem dsps 01ene2022'
+FROM plataformas p
+INNER JOIN cartelera c ON p.id_plataforma = c.id_plataforma
+WHERE p.tipo_suscripcion = 'semestral'
+AND c.fecha > '2022-01-01';
+
++--------------------------------+
+| Precio Prom Sem dsps 01ene2022 |
++--------------------------------+
+|                            130 |
++--------------------------------+
+1 row in set (0.000 sec)
+
+
+/* 13. Mostrar el nombre de la serie con la duración más baja. */
+
+SELECT MIN(duracion) AS 'Duracion + baja', nombre_serie
+FROM series
+GROUP BY nombre_serie DESC
+LIMIT 3;   /* Limito el resultado para ser más preciso */
+
++-----------------+-----------------+
+| Duracion + baja | nombre_serie    |
++-----------------+-----------------+
+|              45 | The Office      |
+|              45 | The Crown       |
+|             110 | Stranger Things |
++-----------------+-----------------+
+3 rows in set (0.000 sec)
+
+
+/* 14. Mostrar todos los datos de todas las tablas de las series protagonizadas por “Penelope Cruz” */
+
+
+
+
+15. Mostrar los datos de los actores que salieron en “Game of Thrones” 
+16. Mostrar el nombre de las series de “HBOM” emitidas después del 01/05/2023 que sean de categoría “INFANTIL” 
+17. Mostrar el precio más bajo de todas las plataformas con Tipo de suscripción “ANUAL”. 
+18. Calcular la media de las temporadas de las series de “TERROR”. 
+19. Contar cuántos actores/actrices estadounidenses de más de 50 años hay. 
+20. Calcular la suma del total de series de la plataforma “RAKU” emitidas. 
+21. Mostrar el nombre de las series protagonizadas por “Scarlet Johansson ” de “Prime Video”. 
+22. Mostrar el nombre del actor/actriz con más series 
+23. Mostrar el nombre y nacionalidad de los actores/actrices de más de 40 años y con más de 30 películas. 
+24. Mostrar los datos de la serie que se emitió en primer lugar ( con la fecha más vieja).
+25. Contar cuantas series están protagonizada por el actor “Daniel Craig
